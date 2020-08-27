676646da4d7dcc513c34d47e53dfc2f2127a8fe7
Dominic <dando.ds11@gmail.com>
2020-08-27 16:23:08 +0300

    Fixed bugs & added spawning and shapes scripts; Fixed bugs about shapes positions and grid; now shapes are moving;

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+    float lastMoveDown = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("a"))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            if(!IsInGrid())
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+
+            Debug.Log(transform.position);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            if (!IsInGrid())
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+
+            Debug.Log(transform.position);
+        }
+
+        if (Input.GetKeyDown("s") || Time.time - lastMoveDown >= 1)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (!IsInGrid())
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+
+            lastMoveDown = Time.time;
+
+            Debug.Log(transform.position);
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            transform.Rotate(0, 0, 90);
+
+            if (!IsInGrid())
+            {
+                transform.Rotate(0, 0, -90);
+            }
+
+            Debug.Log(transform.position);
+        }
+    }
+
+    public bool IsInGrid()
+    {
+        int childCount = 0;
+
+        foreach (Transform childBlock in transform)
+        {
+            Vector2 vect = childBlock.position;
+            childCount++;
+
+            Debug.Log(childCount + " " + childBlock.position);
+
+            if(!IsInBorder(vect))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool IsInBorder(Vector2 pos )
+    {
+        return ((int)pos.x >= -4.31 && (int)pos.x <= 5.69 && (int)pos.y >= -10);
+    }
+
+}
