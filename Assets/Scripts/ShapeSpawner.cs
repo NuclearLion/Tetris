abe41067b5cfca9602f48187d18c17367e968609
Dominic <dando.ds11@gmail.com>
2020-08-28 16:03:44 +0300

    Finished base game; added next shape; fixed bugs - last shape was never spawning, if x layers were destroyed at the same time, score was increased only +1, now it's +x;

--- Assets/Scripts/ShapeSpawner.cs
+++ Assets/Scripts/ShapeSpawner.cs
@@ -6,16 +6,33 @@ public class ShapeSpawner : MonoBehaviour
 {
     public GameObject[] shapes;
 
+    public GameObject[] nextShapes;
+
+    GameObject upNextObject = null;
+
+    int shapeIndex = 0;
+    int nextShapeIndex = 0;
+
     public void SpawnShape()
     {
-        int shapeIndex = Random.Range(0, 7);
+        int shapeIndex = nextShapeIndex;
 
         Instantiate(shapes[shapeIndex], transform.position, Quaternion.identity);
 
+        nextShapeIndex = Random.Range(0, 7);
+
+        Vector3 nextShapePosition = new Vector3(-13, 14, 250);
+
+        if (upNextObject != null)
+            Destroy(upNextObject);
+
+        upNextObject = Instantiate(nextShapes[nextShapeIndex], nextShapePosition, Quaternion.identity);
     }
 
     void Start()
     {
+        nextShapeIndex = Random.Range(0, 7);
+
         SpawnShape();
     }
 
