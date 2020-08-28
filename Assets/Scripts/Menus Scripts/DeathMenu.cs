253d4b6f10b189147663922bf6f091fc46d5a419
Dominic <dando.ds11@gmail.com>
2020-08-28 18:17:29 +0300

    Added menus; added working menus with new "intended feature" on pause menu - resume and quit button aren't working!! Otherwise, the game is finished and bug free;

---
+++ Assets/Scripts/Menus Scripts/DeathMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public string mainMenuSceneToLoad;
+    public string mainSceneToLoad;
+
+    public void Respawn()
+    {
+        SceneManager.LoadScene(mainSceneToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneToLoad);
+    }
+
+}
---
+++ Assets/Scripts/Menus Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //for now, this function will load the actual game;
+    //updates later, it will load a loadign scree;
+    //the loading screen will load the actual game after 2/3f;
+    public void LoadingScreen()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+}
